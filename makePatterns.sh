e629c68e6a292fa09be80227cb6ff57f59e88c17
Jesse Dedoes <INL\does@svowim01.inl.loc>
2012-01-23 15:00:45 +0100

    last commit: Mon Jan 23 15:00:45 CET 2012

---
+++ makePatterns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+pushd $DIR
+export PWD=`pwd`
+export CLASSPATH="$PWD/dist/spellingvariation.jar:$PWD/lib/mysql-connector-java-3.0.17-ga-bin.jar:$PWD/lib/jgrapht-jdk1.6.jar:$PWD/lib/weka.jar:$PWD/lib/commons-cli-1.2.jar:$PWD/lib/commons-logging-1.1.1.jar"
+for x in lib/neo/*.jar;
+do
+#  echo $x;
+  export CLASSPATH="$PWD/$x:$CLASSPATH";
+done
+popd
+java spellingvariation.MultigramTransducer $*
