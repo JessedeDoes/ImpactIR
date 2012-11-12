1cd1f98b08b8da40b23d8b23b08990b450d89573
Jesse Dedoes <INL\does@svowim01.inl.loc>
2012-11-12 11:09:03 +0100

    last commit: Mon Nov 12 11:09:02 CET 2012

---
+++ src/impact/ee/tagger/ChainOfTaggers.java
@@ -0,0 +1,34 @@
+package impact.ee.tagger;
+
+import java.util.*;
+
+public class ChainOfTaggers implements Tagger
+{
+	private List<Tagger> taggers = new ArrayList<Tagger>();
+	
+	@Override
+	public HashMap<String, String> apply(Context c) 
+	{
+		// TODO Auto-generated method stub
+		// this should not be invoked....
+		System.err.println("Never invoke apply on a chain of taggers....");
+		System.exit(1);
+		return null;
+	}
+
+	public void addTagger(Tagger t)
+	{
+		this.taggers.add(t);
+	}
+	@Override
+	public Corpus tag(Corpus inputCorpus) 
+	{
+		// TODO Auto-generated method stub
+		Corpus c = inputCorpus;
+		for (Tagger t: taggers)
+		{
+			c = t.tag(c);
+		}
+		return c;
+	}
+}
