5de7e5b020caf76ba834c75e95d0f53b0feb44b2
Jesse Dedoes <INL\does@svowim01.inl.loc>
2012-11-19 14:53:01 +0100

    last commit: Mon Nov 19 14:52:56 CET 2012

--- src/nl/namescape/tagging/ImpactLemmatizerClient.java
+++ src/nl/namescape/tagging/ImpactLemmatizerClient.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.w3c.dom.Element;
 
 import nl.namescape.filehandling.DirectoryHandling;
+import nl.namescape.filehandling.MultiThreadedFileHandler;
 import impact.ee.lemmatizer.dutch.SimplePatternBasedLemmatizer;
 import impact.ee.lexicon.InMemoryLexicon;
 import impact.ee.tagger.Tagger;
@@ -14,15 +15,19 @@ public class ImpactLemmatizerClient extends ImpactTaggingClient {
 	public ImpactLemmatizerClient(Tagger tagger) 
 	{
 		super(tagger);
+		this.tokenize = false;
 		// TODO Auto-generated constructor stub
 	}
 
 	public void attachToElement(Element e, Map<String,String> m)
 	{
 		// e.setAttribute("type", tag);
-		String tag = m.get("lemma");
-		if (tag != null)
-			e.setAttribute("lemma", tag);
+		if (e.getLocalName().equals("w"))
+		{
+			String tag = m.get("lemma");
+			if (tag != null)
+				e.setAttribute("lemma", tag);
+		}
 	}
 	
 	public static void main(String[] args)
@@ -32,6 +37,8 @@ public class ImpactLemmatizerClient extends ImpactTaggingClient {
 		SimplePatternBasedLemmatizer spbl = new SimplePatternBasedLemmatizer();
 		spbl.train(l);
 		ImpactLemmatizerClient xmlLemmatizer = new ImpactLemmatizerClient(spbl);
-		DirectoryHandling.tagAllFilesInDirectory(xmlLemmatizer, args[1], args[2]);
+		MultiThreadedFileHandler m = new MultiThreadedFileHandler(xmlLemmatizer,4); 
+		DirectoryHandling.tagAllFilesInDirectory(m, args[1], args[2]);
+		m.shutdown();
 	}
 }
