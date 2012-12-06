3624c9e64846cca619b51649ab40ba50d09f726f
Jesse Dedoes <INL\does@svowim01.inl.loc>
2012-12-06 14:26:05 +0100

    last commit: Thu Dec  6 14:25:51 CET 2012

--- src/nl/namescape/evaluation/Counter.java
+++ src/nl/namescape/evaluation/Counter.java
@@ -1,15 +1,20 @@
 package nl.namescape.evaluation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Counter<T> 
+import nl.namescape.stats.WordList.TypeFrequency;
+
+public class Counter<T> extends HashMap<T,Integer>
 {
-	private Map<T,Integer> map = new HashMap<T,Integer>();
-	
-	public int get(T key)
+	@Override
+	public Integer get(Object key)
 	{
-		Integer z = map.get(key);
+		Integer z = super.get(key);
 		if (z==null)
 			return 0;
 		return z;
@@ -18,6 +23,38 @@ public class Counter<T>
 	public void increment(T key)
 	{
 		Integer z = get(key);
-		map.put(key, z+1);
+		super.put(key, z+1);
+	}
+	
+	public void increment(T key, int amount)
+	{
+		Integer z = get(key);
+		super.put(key, z+amount);
+	}
+	
+	public class CompareCounts implements Comparator<T> 
+	{
+		public int compare(T a, T b) 
+		{
+			if(get(a) < get(b)) 
+			{
+				return 1;
+			} else if(get(a)== get(b)) 
+			{
+				return 0;
+			} else 
+			{
+				return -1;
+			}
+		}
+	}
+	
+	public List<T> keyList()
+	{
+		List<T> l = new ArrayList<T>();
+		l.addAll(this.keySet());
+		CompareCounts v =   new CompareCounts();
+		Collections.sort(l, v);
+		return l;
 	}
 }
--- src/nl/namescape/languageidentification/LanguageTagger.java
+++ src/nl/namescape/languageidentification/LanguageTagger.java
@@ -1,4 +1,5 @@
 package nl.namescape.languageidentification;
+import nl.namescape.evaluation.Counter;
 import nl.namescape.filehandling.DirectoryHandling;
 import nl.namescape.filehandling.SimpleInputOutputProcess;
 import nl.namescape.sentence.JVKSentenceSplitter;
@@ -21,13 +22,27 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.*;
 
+/**
+ * Simple paragraph-level language identification<br>
+ * Now using cybozu labs lamguage identification
+ * <p>
+ * Problems:
+ *<p>
+ * Not very good at short paragraphs<br>
+ * Uppercase-only text should be lowercased (else almost always recognized as german)
+ * 
+ * @author does
+ *
+ */
 public class LanguageTagger implements SimpleInputOutputProcess
 {
+	
 	static
 	{
 		try 
 		{
 			DetectorFactory.loadProfileFromJar();
+			List<String> langs = DetectorFactory.getLangList();
 		} catch (LangDetectException e) 
 		{
 			// TODO Auto-generated catch block
@@ -46,46 +61,89 @@ public class LanguageTagger implements SimpleInputOutputProcess
 			return lang;
 		} catch (Exception e)
 		{
-			e.printStackTrace();
+			// e.printStackTrace();
 			return null;
 		}
 	}
 
-	public void tagLanguages(Document d)
+	public String tagLanguages(Document d)
 	{
+		Counter<String> c = new Counter<String>();
 		Set<Element> paragraphLike = TEITagClasses.getSentenceSplittingElements(d);
+		int L=0;
 		for (Element  z: paragraphLike)
 		{
+			// System.err.println(z);
+			
 			String s = z.getTextContent();
-			System.err.println("paragraph content: " + s);
+			L += s.length();
+			
+			// System.err.println("paragraph content: " + s);
+			
 			String lang = detectLanguage(s);
+			
 			if (lang != null)
-				z.setAttribute("lang", lang);
+			{
+				if (lang.equals("af"))
+					lang = "nl";
+				z.setAttribute("xml:lang", lang);
+				c.increment(lang,s.length());
+				
+				if (!lang.equalsIgnoreCase("nl") && s.length() > 100)
+				{
+					// System.err.println(lang + " IN " + s);
+				}
+			} else
+			{	
+				// System.err.println("No language found for " + s);
+			}
 		}
+		
+		String mainLanguage = "unknown";
+		
+		for (String lang: c.keyList())
+		{
+			System.err.println(lang + "\t"  + c.get(lang));
+			if (c.get(lang) > 0.5  *L)
+			{
+				mainLanguage = lang;
+				d.getDocumentElement().setAttribute("xml:lang", mainLanguage);
+				if (!lang.equalsIgnoreCase("nl"))
+				{
+					System.err.println("Document has nondutch main lang: "  + lang);
+				}
+			}
+		}
+		
+		if (mainLanguage.equals("unknown"))
+		{
+			System.err.println("No main language found! Text length in chars: " + L);
+		}
+		return mainLanguage;
 	}
 	
 	@Override
 	public void handleFile(String in, String out) 
 	{
-
+		
 		Document d = null;
-		
-			try 
+		try 
+		{
+			d = XML.parse(in);
+			String main = tagLanguages(d);
+			if (!main.equals("nl"))
 			{
-				d = XML.parse(in);
-				tagLanguages(d);
-			} catch (Exception e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return;
-			} 
-		
+				System.err.println("Nondutch doc: " + main + " : "  + in);
+			}
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+			return;
+		} 
+
 		try 
 		{
 			PrintStream pout = new PrintStream(new FileOutputStream(out));
-			
-			
 			pout.print(XML.documentToString(d));
 			pout.close();
 		} catch (Exception e) 
