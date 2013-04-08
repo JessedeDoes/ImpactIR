e251ad9e0a29962261422bb2f0b4ac8f380c35fa
Jesse Dedoes <INL\does@svowim01.inl.loc>
2013-04-08 23:38:42 +0200

    last commit: Mon Apr  8 23:38:42 CEST 2013

--- src/impact/ee/tagger/LookupLemmatizer.java
+++ src/impact/ee/tagger/LookupLemmatizer.java
@@ -49,6 +49,7 @@ public class LookupLemmatizer implements Tagger
 			ArrayList<WordMatch> asList = new ArrayList<WordMatch>(s);
 			Collections.sort(asList, new WordMatchComparator());
 			WordMatch bestMatch = asList.get(0);
+			System.err.println(w + "-->" + bestMatch.wordform);
 			m.put("lemma", bestMatch.wordform.lemma);
 			//test.incrementCount(bestMatch.type);
 			/*
