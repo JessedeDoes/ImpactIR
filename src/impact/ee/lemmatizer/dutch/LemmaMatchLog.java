c2d78350a73d679d1d7305427c937e3a93d223f9
jesse <jesse.dedoes@inl.nl>
2013-10-10 22:09:20 +0200

    last commit: do okt 10 22:09:19 CEST 2013

--- src/impact/ee/lemmatizer/dutch/LemmaMatchLog.java
+++ src/impact/ee/lemmatizer/dutch/LemmaMatchLog.java
@@ -2,14 +2,15 @@ package impact.ee.lemmatizer.dutch;
 import impact.ee.lemmatizer.dutch.LemmaMatch.MatchType;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import nl.namescape.evaluation.Counter;
 
 public class LemmaMatchLog 
 {
-	Map<String, Set<LemmaMatch>> log = new HashMap<String, Set<LemmaMatch>>();
+	Map<String, Set<LemmaMatch>> log = new ConcurrentHashMap<String, Set<LemmaMatch>>();
 	
-	void addToLog(String wordform, String lemma, String lexiconTag, String corpusTag, MatchType type)
+	synchronized void addToLogAction(String wordform, String lemma, String lexiconTag, String corpusTag, MatchType type)
 	{
 		String key = wordform + ":" + corpusTag;
 		
@@ -34,10 +35,35 @@ public class LemmaMatchLog
 		V.add(lm);
 	}
 	
-	Set<LemmaMatch> getLoggedMatches(String wordform, String corpusTag)
+	synchronized void addToLog(String wordform, String lemma, String lexiconTag, String corpusTag, MatchType type)
+	{
+		logAction(wordform,lemma,lexiconTag,corpusTag,type,true);
+	}
+	
+	Set<LemmaMatch> getLoggedMatchesAction(String wordform, String corpusTag)
 	{
 		String key = wordform + ":" + corpusTag;
 		return log.get(key);
+	}
+	
+
+	
+	Set<LemmaMatch> getLoggedMatches(String wordform, String corpusTag)
+	{
+		String key = wordform + ":" + corpusTag;
+		return logAction(wordform,null,null,corpusTag,MatchType.Unknown,false);
+	}
+	
+	/* to prevent simultaneous getting and setting ... */
+	
+	synchronized Set<LemmaMatch> logAction(String wordform, String lemma, String lexiconTag, String corpusTag, MatchType type, boolean store)
+	{
+		if (!store)
+		{
+			return getLoggedMatchesAction(wordform, corpusTag);
+		}
+		addToLogAction(wordform,lemma,lexiconTag,corpusTag,type);
+		return null;
 	}
 	
 	Counter<String> getGuesserPoSMatches()
